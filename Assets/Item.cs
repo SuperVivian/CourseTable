1099b87c1aedba5ba50388783628670f65c6da13
SuperVivian <1113679022@qq.com>
2020-09-01 10:17:28 +0800

    实现天数计算

---
+++ Assets/Item.cs
@@ -0,0 +1,35 @@
+﻿//==========================
+// - FileName:      Item.cs         
+// - Created:       #AuthorName#	
+// - CreateTime:    #CreateTime#	
+// - Description:   
+//==========================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    Button btn;
+    InputField allTasksNum;
+    InputField dailyTasksNum;
+    Text daysCount;
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnBtnDown);
+
+        allTasksNum = transform.Find("all").GetComponent<InputField>();
+        dailyTasksNum = transform.Find("daily").GetComponent<InputField>();
+        daysCount = transform.Find("days").GetComponent<Text>();
+    }
+
+    public void OnBtnDown()
+    {
+        Debug.Log("按下item");
+        ItemManager.allTasksNum = allTasksNum;
+        ItemManager.dailyTasksNum = dailyTasksNum;
+        ItemManager.daysCount = daysCount;
+    }
+}
---
+++ Assets/ItemManager.cs
@@ -0,0 +1,38 @@
+﻿//==========================
+// - FileName:      ItemManager.cs         
+// - Created:       fww	
+// - CreateTime:    2020.9.1
+// - Description:   
+//==========================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+
+public class ItemManager : MonoBehaviour
+{
+    public static InputField allTasksNum;
+    public static InputField dailyTasksNum;
+    public static Text daysCount;
+
+
+    public void OkBtnDown()
+    {
+        Debug.Log("按下ok");
+        float all = Convert.ToInt32(allTasksNum.text);
+        float daily = Convert.ToInt32(dailyTasksNum.text);
+        int count = Convert.ToInt32(Mathf.Ceil(all/ daily));
+        daysCount.text = "需要" + count.ToString() + "天完成！";
+    }
+    public void ClearBtnDown()
+    {
+
+    }
+    public void AddBtnDown()
+    {
+
+    }
+
+}
